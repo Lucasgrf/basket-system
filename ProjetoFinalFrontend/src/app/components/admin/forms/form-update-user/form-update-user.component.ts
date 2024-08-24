a5c4456d1f640f27952398c4d2e9f3562cd5069c
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-08-24 06:19:36 -0300

    feat: users crud frontend

--- ProjetoFinalFrontend/src/app/components/admin/forms/form-update-user/form-update-user.component.ts
+++ ProjetoFinalFrontend/src/app/components/admin/forms/form-update-user/form-update-user.component.ts
@@ -1,17 +1,57 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../../services/user.service';
+import { HeaderComponent } from "../../header/header.component";
 
 @Component({
   selector: 'app-form-update-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, HeaderComponent],
   templateUrl: './form-update-user.component.html',
   styleUrl: './form-update-user.component.scss'
 })
-export class FormUpdateUserComponent {
-  user: any = {};
+export class FormUpdateUserComponent implements OnInit {
+  userForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.loadUser(id);
+  }
+
+  loadUser(id: number) {
+    // Fetch user data and patch the form
+    this.userService.getUserById(id).subscribe(user => {
+      this.userForm.patchValue(user);
+    });
+  }
 
   onSubmitUser() {
-    throw new Error('Method not implemented.');
+    if (this.userForm.valid) {
+      const id: number = +this.route.snapshot.paramMap.get('id')!;
+      this.userService.updateUser(id, this.userForm.value).subscribe({
+        next: (user) => {
+          console.log('Usuário atualizado com sucesso', user);
+          alert('Usuário atualizado com sucesso');
+        },
+        error: (error) => {
+          console.error('Erro ao atualizar usuário', error);
+          alert('Erro ao atualizar usuário');
+        }
+      });
+    }
   }
 }
